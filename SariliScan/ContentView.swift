7eb544b02aed6c620761ef553a8d2f5410dc47d1
Simon Clendinning <sclendinning@gmail.com>
2026-06-24 12:06:53 +0700

    Initial Commit

---
+++ SariliScan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SariliScan
+//
+//  Created by Tez on 24/6/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SariliScan/SariliScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SariliScanApp.swift
+//  SariliScan
+//
+//  Created by Tez on 24/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct SariliScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
